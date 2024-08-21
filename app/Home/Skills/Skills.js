20d9c7285e113038eeb6f98b416e836e31de12e8
kevthefoo <kevthefoo@gmail.com>
2024-08-22 08:22:59 +1000

    skills section progress 85%

--- app/Home/Skills/Skills.js
+++ app/Home/Skills/Skills.js
@@ -1,61 +1,99 @@
+import "./skills.css";
+import { HiAcademicCap } from "react-icons/hi2";
+import { FaAppleAlt } from "react-icons/fa";
 export default function Skills() {
   return (
     <section id="skills" className="flex-col justify-start pt-16">
       <h1>Skills</h1>
-      <span className="text-xl text-gray-500">What I got in my bag</span>
-      <div className="grid grid-cols-2 items-stretch gap-x-16 gap-y-10 justify-center border-2 border-red-700">
-        <div className="flex flex-col rounded-xl border-2 border-black">
-          <h3>Frontend</h3>
-          <ul>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>Tailwindcss</li>
-            <li>React</li>
-          </ul>
+      <span className="mb-4 text-xl text-gray-500">What I got in my bag</span>
+      <div className="grid h-full grid-cols-2 justify-center gap-8">
+        <div className="flex w-80 flex-col rounded-xl border-2 border-gray-400 bg-slate-200 p-4 dark:bg-slate-800">
+          <h3 className="mb-4 self-center text-xl">Frontend</h3>
+          <div>
+            <ul className="grid grid-flow-col grid-cols-2 grid-rows-4 items-center justify-center gap-4">
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                React
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Next.js
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Tailwindcss
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                JavaScript
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className="flex flex-col rounded-xl border-2 border-black">
-          <h3>Backend</h3>
-          <ul>
-            <li>Python</li>
-            <li>Node.js</li>
-            <li>Mongodb</li>
-            <li>MySQL</li>
-            <li>AWS</li>
-          </ul>
+        <div className="flex w-80 flex-col rounded-xl border-2 border-gray-400 bg-slate-200 p-4 dark:bg-slate-800">
+          <h3 className="mb-4 self-center text-xl">Backend</h3>
+          <div>
+            <ul className="grid grid-flow-col grid-cols-2 grid-rows-4 items-center justify-center gap-4">
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Python
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Node.js
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Mongodb
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                MySQL
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                AWS
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className="flex flex-col rounded-xl border-2 border-black">
-          <h3>Blockchain</h3>
-          <ul>
-            <li>Solidity</li>
-            <li>Ethers.js</li>
-            <li>Web3.js</li>
-            <li>Truffle</li>
-          </ul>
+        <div className="flex w-80 flex-col rounded-xl border-2 border-gray-400 bg-slate-200 p-4 dark:bg-slate-800">
+          <h3 className="mb-4 self-center text-xl">Blockchain</h3>
+          <div>
+            <ul className="grid grid-flow-col grid-cols-2 grid-rows-4 items-center justify-center gap-4">
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Solidity
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Truffle
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Web3.js
+              </li>
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Ether.js
+              </li>
+            </ul>
+          </div>
         </div>
 
-        
-        <div className="flex flex-col rounded-xl border-2 border-black">
-          <h3>Others</h3>
-          <ul>
-            <li>Figma</li>
-
-          </ul>
+        <div className="flex w-80 flex-col rounded-xl border-2 border-gray-400 bg-slate-200 p-4 dark:bg-slate-800">
+          <h3 className="mb-4 self-center text-xl">Others</h3>
+          <div>
+            <ul className="grid grid-flow-col grid-cols-2 grid-rows-4 items-center justify-center gap-4">
+              <li className="flex items-center justify-start gap-1">
+                <HiAcademicCap />
+                Figma
+              </li>
+            </ul>
+          </div>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
     </section>
   );
