7d89d8e27cd5b7135a0f7ab3e837669ef393947a
KevTheFoo <kevthefoo@gmail.com>
2024-09-03 12:31:31 +1000

    refactor: Update Header component with responsive styling

--- app/Component/Header/Header.js
+++ app/Component/Header/Header.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import { FaLinkedin, FaGithubSquare, FaMedium } from "react-icons/fa";
 import ThemeToggle from "@/app/Component/ThemeToggle/ThemeToggle";
 
 export default function Header() {
+  const [active, setActive] = useState(false);
   return (
-    <header className="fixed z-50 flex h-16 w-full max-md:border-2 max-md:border-red-500 items-center justify-around border-b-2 bg-white transition duration-700 ease-linear dark:bg-neutral-900 dark:text-white">
+    <header className="fixed z-50 flex h-16 w-full items-center justify-around border-b-2 bg-white transition duration-700 ease-linear max-md:border-2 max-md:border-red-500 dark:bg-neutral-900 dark:text-white">
       <div>Logo</div>
       <nav className="border-2 border-red-500 max-md:hidden">
         <ul className="flex items-center justify-between gap-4">
@@ -25,7 +27,7 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <div className="flex items-center justify-between gap-4">
+      <div className="flex items-center justify-between gap-4 border-2 border-red-500">
         <ul className="flex items-center justify-between gap-2">
           <li>
             <a href="https://github.com" target="_blank" title="Github">
@@ -53,6 +55,61 @@ export default function Header() {
         </ul>
         <ThemeToggle />
       </div>
+
+      <div
+        className="relative z-50 hidden h-7 w-7 cursor-pointer flex-col justify-between max-md:flex"
+        onClick={() => setActive(!active)}
+      >
+        <div
+          className={
+            active
+              ? "absolute left-0 top-1/2 h-1 w-full rotate-45 bg-black transition ease-in-out"
+              : "h-1 w-full bg-black transition ease-in-out"
+          }
+        ></div>
+        <div
+          className={
+            active ? "" : "h-1 w-full bg-black transition delay-150 ease-in-out"
+          }
+        ></div>
+        <div
+          className={
+            active
+              ? "absolute left-0 top-1/2 h-1 w-full rotate-[-45deg] bg-black transition ease-in-out"
+              : "h-1 w-full bg-black transition ease-in-out"
+          }
+        ></div>
+      </div>
+
+      <nav
+        className={
+          active
+            ? "fixed left-0 top-0 z-30 flex h-screen w-full items-center justify-center border-2 border-blue-500 bg-slate-300"
+            : "fixed left-full top-0 z-30 flex h-screen w-full items-center justify-center border-2 border-blue-500 bg-slate-300"
+        }
+      >
+        <ul className="mobile_menu">
+          <li>
+            <a href="#features" onClick={() => setActive(false)}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#pricing" onClick={() => setActive(false)}>
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://discord.gg/qVVFgB9UPy"
+              target="_blank"
+              onClick={() => setActive(false)}
+            >
+              we
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
