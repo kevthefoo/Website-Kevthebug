89971537b38fce7769b136920ba4d24c86f18f59
KevTheFoo <kevthefoo@gmail.com>
2024-09-06 23:19:51 +1000

    Refactor section padding in Showcase component

--- app/Home/Showcase/Showcase.js
+++ app/Home/Showcase/Showcase.js
@@ -4,7 +4,7 @@ export default function Showcase() {
   return (
     <section
       id="showcase"
-      className="flex-col justify-start border-2 border-red-300 pb-16 pt-16"
+      className="flex-col justify-start pb-16 pt-16"
     >
       <h1>Showcase</h1>
       <span className="mb-4 text-xl text-gray-500">My Highlight Projects</span>
