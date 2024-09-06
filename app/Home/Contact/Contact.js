290b26721baaab347d1cf11b32a0619de7128289
KevTheFoo <kevthefoo@gmail.com>
2024-09-06 21:27:14 +1000

    Refactor responsiveness and update styling in Carousel component

--- app/Home/Contact/Contact.js
+++ app/Home/Contact/Contact.js
@@ -9,38 +9,37 @@ export default function Conatct() {
   return (
     <section id="contact" className="h-auto flex-col justify-start pb-24 pt-16">
       <h1>Contact</h1>
+
       <span className="mb-4 text-xl text-gray-500">Get in touch with me</span>
-      <div className="flex h-full w-full items-stretch justify-center gap-10">
-        <div className="flex w-1/5 flex-col items-stretch justify-between gap-4">
+
+      <div className="flex h-full w-full items-stretch justify-center gap-10 max-sm:flex-col">
+        <div className="flex flex-col items-stretch justify-between gap-4 max-sm:flex-row max-sm:justify-center max-sm:gap-2">
           <a
             href="mailto:kevthebug@gmail.com"
-            className="flex flex-grow flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 dark:bg-slate-800 dark:hover:bg-slate-700"
+            className="flex aspect-square flex-1 flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 max-md:p-0 dark:bg-slate-800 dark:hover:bg-slate-700"
           >
-            <MdEmail className="mb-2 text-3xl" />
-            <h3 className="text-xl">Email</h3>
+            <MdEmail className="mb-2 text-3xl max-md:text-xl" />
+            <h3 className="text-xl max-[992px]:text-[12px]">Email</h3>
           </a>
           <a
             href="https://www.linkedin.com/in/kevthebug/"
             target="_blank"
-            className="flex flex-grow flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 dark:bg-slate-800 dark:hover:bg-slate-700"
+            className="flex aspect-square flex-1 flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 max-md:p-0 dark:bg-slate-800 dark:hover:bg-slate-700"
           >
-            <FaLinkedin className="mb-2 text-3xl" />
-            <h3 className="text-xl">LinkedIn</h3>
+            <FaLinkedin className="mb-2 text-3xl max-md:text-xl" />
+            <h3 className="text-xl max-[992px]:text-[12px]">LinkedIn</h3>
           </a>
           <a
             href="https://maps.app.goo.gl/srk7XxEz989FP3Ws8"
             target="_blank"
-            className="flex flex-grow flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 dark:bg-slate-800 dark:hover:bg-slate-700"
+            className="flex aspect-square flex-1 flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 px-8 py-4 max-md:p-0 dark:bg-slate-800 dark:hover:bg-slate-700"
           >
-            <FaLocationDot className="mb-2 text-3xl" />
-            <h3 className="text-xl">Location</h3>
+            <FaLocationDot className="mb-2 text-3xl max-md:text-xl" />
+            <h3 className="text-xl max-[992px]:text-[12px]">Location</h3>
           </a>
         </div>
 
-        <form
-          action=""
-          className="w-2/5 rounded-3xl border-2 border-gray-400 p-8"
-        >
+        <form action="" className="rounded-3xl border-2 border-gray-400 p-8">
           <div className="mb-8 flex flex-col items-start justify-start">
             <label htmlFor="name" className="mb-2">
               Name:
