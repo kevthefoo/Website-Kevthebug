07a862e226ca8cc71e74850bf1e749b47fbe4ffa
KevTheFoo <kevthefoo@gmail.com>
2024-09-06 23:05:06 +1000

    Refactor image paths in Carousel component and adjust slide height

--- app/Home/Showcase/Showcase.js
+++ app/Home/Showcase/Showcase.js
@@ -2,7 +2,7 @@ import Carousel from "@/app/Component/Carousel/Carousel";
 
 export default function Showcase() {
   return (
-    <section id="showcase" className="h-screen flex-col justify-start pt-16">
+    <section id="showcase" className="min-h-screen flex-col justify-start pt-16">
       <h1>Showcase</h1>
       <span className="text-xl text-gray-500 mb-4">My Highlight Projects</span>
       <Carousel />
