9f42774d130f49a2b4c21b0e36002b457ea7096f
KevTheFoo <kevthefoo@gmail.com>
2024-09-06 20:48:33 +1000

    Refactor responsiveness and update styling in Services component

--- app/Home/Services/Services.js
+++ app/Home/Services/Services.js
@@ -12,37 +12,37 @@ export default function Services() {
   return (
     <section
       id="services"
-      className="h-screen flex-col justify-start pb-16 pt-16"
+      className="min-f-screen flex-col justify-start pb-16 pt-16"
     >
       <h1>Services</h1>
       <span className="mb-4 text-xl text-gray-500">What I can provide</span>
-      <div className="grid h-full grid-cols-3 gap-20">
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+      <div className="grid h-full grid-cols-3 gap-20 border-2 border-blue-300 text-center max-[992px]:gap-10 max-md:grid-cols-2">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaReact className="mb-4 text-5xl" />
           <h3>Website Development</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaFigma className="mb-4 text-5xl" />
           <h3>UI/UX Design</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaShopify className="mb-4 text-5xl" />
           <h3>E-commerce Development</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaSignal className="mb-4 text-5xl" />
           <h3>SEO</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaDiscord className="mb-4 text-5xl" />
           <h3>Custom Discord Bot</h3>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
+        <div className="flex aspect-square flex-col items-center justify-center rounded-3xl border-2 border-gray-400 bg-slate-200 p-2 dark:border-white dark:bg-black">
           <FaEthereum className="mb-4 text-5xl" />
           <h3>Smart Contract Development</h3>
         </div>
